26cbe697c8ffac5daff6b48f989c69aa8b384979
Denis-7242 <dexdenis3@gmail.com>
2025-11-26 13:09:53 +0300

    updated some lines of comments

--- 101. Symmetric Tree/TreeNode.java
+++ 101. Symmetric Tree/TreeNode.java
@@ -1,7 +1,5 @@
-/**
- * Definition for a binary tree node.
- */
-public class TreeNode {
+//Definition for a binary tree node.
+ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
